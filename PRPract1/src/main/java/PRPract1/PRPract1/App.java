733c6c157b623014863487aa69a61add0b0798eb
qadhanashree <d.amol.gawde@accenture.com>
2023-11-13 12:54:43 +0530

    First commit test...

---
+++ PRPract1/src/main/java/PRPract1/PRPract1/App.java
@@ -0,0 +1,13 @@
+package PRPract1.PRPract1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
